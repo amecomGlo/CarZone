401110670046d4d1c078362c2dbeb9afbb7d66fe
Meshach Akakah <meshlrd14@outlook.com>
2023-05-09 22:37:50 +0100

    css

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -1,15 +1,15 @@
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-23-ravd(^4yb(!7zzjd4flmb+cnl_wd&ynw!8+obl$-&tu$pzo'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['mighty-citadel-94628.herokuapp.com']
+ALLOWED_HOSTS = [' Carzone.pythonanywhere.com']
 LOGIN_REDIRECT_URL = '/'
 
 
@@ -73,26 +73,26 @@ WSGI_APPLICATION = 'CarZone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:Happyzone23@@@localhost/carzone_db')}
+# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:Happyzone23@@@localhost/carzone_db')}
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'carzone_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Happyzone23@@',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Happyzone23@@',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
