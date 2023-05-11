45b677212f5aca58a63e72dcb236f4fa9243128a
Meshach Akakah <meshlrd14@outlook.com>
2023-05-11 01:23:03 +0100

    'okay'

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'CarZone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path.joinpath(BASE_DIR, 'templates/')],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
